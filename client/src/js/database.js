15341f2a26e73f504b32fdfd3b4fbc1403287396
Balalomba Barandao <barand14@gmail.com>
2024-07-18 14:46:20 -0400

    put method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,20 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  const textDb = await openDB('jate', 1);
 
+  // Create a new transaction and specify the database and data privileges.
+  const tx = textDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+ 
+  const request = store.put({id:1, value: content});
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result.value);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
